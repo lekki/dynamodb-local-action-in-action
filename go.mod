db7ff239a9aa34589ac978a889e079ba0985389f
Pawel Lekki <pawel.lekki@gmail.com>
2019-09-11 23:15:46 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lekki/action-test
+
+go 1.12
+
+require github.com/aws/aws-sdk-go-v2 v0.0.0-20190104002603-381987c30353
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"log"
+)
+
+func main() {
+
+	fmt.Println("hello world")
+
+	svc := dynamoDb()
+
+	res, err := svc.ListTablesRequest(&dynamodb.ListTablesInput{}).Send()
+	fmt.Println(err)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(res.TableNames)
+}
+
+func dynamodbConfig(baseConfig aws.Config) aws.Config {
+
+	baseConfig.EndpointResolver = aws.ResolveWithEndpointURL("http://localhost:8000")
+
+	//baseConfig.LogLevel = aws.LogDebugWithHTTPBody
+	return baseConfig
+}
+
+func dynamoDb() *dynamodb.DynamoDB {
+	d := dynamodb.New(dynamodbConfig(loadBasicAwsConfig()))
+	return d
+}
+
+func loadBasicAwsConfig() aws.Config {
+	cfg, err := external.LoadDefaultAWSConfig()
+	cfg.LogLevel = aws.LogDebugWithHTTPBody
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return cfg.Copy()
+}
